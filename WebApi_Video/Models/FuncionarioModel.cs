27e41c2f192c0a30286b4f4fa02b628e8b328cd7
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-14 14:41:51 -0300

    Criando Relação no FunciorioModel com Enums

--- WebApi_Video/Models/FuncionarioModel.cs
+++ WebApi_Video/Models/FuncionarioModel.cs
@@ -1,13 +1,15 @@
-﻿namespace WebApi_Video.Models
+﻿using WebApi_Video.Enums;
+
+namespace WebApi_Video.Models
 {
     public class FuncionarioModel
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome  { get; set; }
-        public string Departamento  { get; set; }
+        public DepartamentoEnum Departamento {  get; set; }
         public bool Ativo { get; set; }
-        public string Turno { get; set;}
+        public TurnoEnum Turno { get; set; }
         public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
         public DateTime DataDeAlteracao { get;set; } = DateTime.Now.ToLocalTime();
     }
