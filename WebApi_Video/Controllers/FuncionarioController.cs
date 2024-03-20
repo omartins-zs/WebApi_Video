1565cd9144da5d737dffda56b1881dc1269d2ea2
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-20 14:14:25 -0300

    Criando Controller de Api dos Funcionarios

---
+++ WebApi_Video/Controllers/FuncionarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Video.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuncionarioController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("OI");
+        }
+    }
+}
