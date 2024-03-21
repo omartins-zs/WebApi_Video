554ef66a9187ad3754b98ca9dae60e0d86bed9d3
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-21 08:05:55 -0300

    Retirando List do GetFuncionarioById na Interface

--- WebApi_Video/Service/FuncionarioService/IFuncionarioInterface.cs
+++ WebApi_Video/Service/FuncionarioService/IFuncionarioInterface.cs
@@ -6,7 +6,7 @@ namespace WebApi_Video.Service.FuncionarioService
     {
         Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios();
         Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionario(FuncionarioModel novoFuncionario);
-        Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarioById(int id);
+        Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id);
         Task<ServiceResponse<List<FuncionarioModel>>> UpdateFuncionario(FuncionarioModel editadoFuncionario);
         Task<ServiceResponse<List<FuncionarioModel>>> DeleteFuncionario(int id);
         Task<ServiceResponse<List<FuncionarioModel>>> InativaFuncionario(int id);
